97fdd529a60e891fae6f5257cac1a507a76179c7
Alexis <alexis.pellicier@nds.k12.tr>
2018-10-30 10:41:47 +0300

    1srt

---
+++ config.js
@@ -0,0 +1,33 @@
+/*
+ *Create and export configuration variables
+ *
+ */
+
+ // Container for all the environments
+ let environments = {};
+
+ // Staging (default) environment
+ environments.staging = {
+   'httpPort' : 3000,
+   'httpsPort' : 3001,
+   'envName' : 'staging'
+ };
+
+
+ // Production environment
+ environments.production = {
+   'httpPort' : 5000,
+   'httpsPort' : 5001,
+   'envName' : 'production'
+
+ }
+
+// Determine which environment was passed as a command-line argument
+let currentenvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the environment is one oh those above or default to staging
+let environmentToExport = typeof(environments[currentenvironment]) === 'object' ? environments[currentenvironment] : environments.staging;
+
+//Export the module
+
+module.exports = environmentToExport;
